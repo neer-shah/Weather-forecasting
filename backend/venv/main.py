4f9697f8238fcbdc4d836b4a64f3d78d41a218e2
N1049041 <neer.shah2021@my.ntu.ac.uk>
2026-02-25 14:19:20 +0000

    Fixing CORS

--- backend/venv/main.py
+++ backend/venv/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Query
 from datetime import datetime, timezone, timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 # website /health
 @app.get("/health")
